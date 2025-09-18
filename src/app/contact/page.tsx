5d01cb5d7e2e33eaa4e1c10afce41f8f2f5c6b15
Duong Nam <dnam281224@gmail.com>
2025-09-19 00:02:50 +0700

    contact UI

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,40 @@
-import React from "react";
-
+import Button from "@/components/Button/Button";
+import Image from "next/image";
 const page = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="w-full flex flex-col items-center">
+      <h1>Let&apos;s Keep in Touch</h1>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-20 mt-10">
+        <div className="w-full h-100 relative">
+          <Image
+            src={"/contact.png"}
+            fill={true}
+            alt={"contact"}
+            className="object-contain moving"
+          />
+        </div>
+
+        <form action="" className="w-1/2 flex flex-col gap-5">
+          <input
+            type="text"
+            className="py-2 px-4 w-100 text-xl border border-black rounded-sm placeholder:text-base"
+            placeholder="Name"
+          />
+          <input
+            type="email"
+            className="py-2 px-4 w-100 text-xl border border-black rounded-sm placeholder:text-base"
+            placeholder="Email"
+          />
+          <textarea
+            className="p-2 w-100 h-50 border border-black rounded-sm"
+            placeholder="Message"
+          ></textarea>
+          <Button name="Send" link="/" className="btn-primary" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default page;
