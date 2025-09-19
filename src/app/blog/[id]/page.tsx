dbf527f65f4d467fac2e766afd8176e3e05ccd1d
Duong Nam <dnam281224@gmail.com>
2025-09-19 16:41:42 +0700

    blog UI

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Image from "next/image";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="flex gap-10 items-center">
+        <div className="flex flex-col  gap-12 w-1/2">
+          <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsa?
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo amet
+            ullam obcaecati fugit quod. Magni cum sed odio minus nesciunt
+            consequatur perferendis distinctio, hic laudantium?Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Consequuntur, mollitia?Lorem,
+            ipsum dolor sit amet consectetur adipisicing elit. Unde facilis
+          </p>
+        </div>
+
+        <div className="w-1/2 h-80 relative ">
+          <Image
+            src={
+              "https://cdn.pixabay.com/photo/2015/08/28/11/37/painting-911804_1280.jpg"
+            }
+            alt="image"
+            fill={true}
+            className="object-center object-cover"
+          />
+        </div>
+      </div>
+
+      <div className=""></div>
+    </div>
+  );
 };
 
 export default page;
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,70 +1,78 @@
 import Image from "next/image";
+import Link from "next/link";
 const page = () => {
   return (
     <div>
-      <div className="flex gap-10 mt-15">
-        <div className="order-1">
-          <div className="flex flex-col gap-5">
-            <h1>Creative Portfolio</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-              autem, quo voluptatem eligendi repellendus possimus quibusdam.
-              Corrupti debitis at accusamus.
-            </p>
+      <Link href="blog/1">
+        <div className="flex gap-10 mt-15">
+          <div className="order-1">
+            <div className="flex flex-col gap-5">
+              <h1>Creative Portfolio</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+                autem, quo voluptatem eligendi repellendus possimus quibusdam.
+                Corrupti debitis at accusamus.
+              </p>
+            </div>
+          </div>
+
+          <div className="w-100 lg:h-50 h-40 relative max-sm:hidden">
+            <Image
+              src="https://cdn.pixabay.com/photo/2017/03/01/11/15/paint-2108185_1280.jpg"
+              alt="image"
+              fill={true}
+              className=""
+            />
           </div>
         </div>
+      </Link>
 
-        <div className="w-100 lg:h-50 h-40 relative max-sm:hidden">
-          <Image
-            src="https://cdn.pixabay.com/photo/2017/03/01/11/15/paint-2108185_1280.jpg"
-            alt="image"
-            fill={true}
-            className=""
-          />
-        </div>
-      </div>
-      <div className="flex gap-10 mt-15">
-        <div className="order-1">
-          <div className="flex flex-col gap-5">
-            <h1>Creative Portfolio</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-              autem, quo voluptatem eligendi repellendus possimus quibusdam.
-              Corrupti debitis at accusamus.
-            </p>
+      <Link href="blog/2">
+        <div className="flex gap-10 mt-15">
+          <div className="order-1">
+            <div className="flex flex-col gap-5">
+              <h1>Creative Portfolio</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+                autem, quo voluptatem eligendi repellendus possimus quibusdam.
+                Corrupti debitis at accusamus.
+              </p>
+            </div>
+          </div>
+
+          <div className="w-100 lg:h-50 h-40 relative max-sm:hidden">
+            <Image
+              src="https://cdn.pixabay.com/photo/2015/08/28/11/37/painting-911804_1280.jpg"
+              alt="image"
+              fill={true}
+              className=""
+            />
           </div>
         </div>
+      </Link>
+      <Link href="blog/3">
+        <div className="flex gap-10 mt-15">
+          <div className="order-1">
+            <div className="flex flex-col gap-5">
+              <h1>Creative Portfolio</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+                autem, quo voluptatem eligendi repellendus possimus quibusdam.
+                Corrupti debitis at accusamus.
+              </p>
+            </div>
+          </div>
 
-        <div className="w-100 lg:h-50 h-40 relative max-sm:hidden">
-          <Image
-            src="https://cdn.pixabay.com/photo/2015/08/28/11/37/painting-911804_1280.jpg"
-            alt="image"
-            fill={true}
-            className=""
-          />
-        </div>
-      </div>
-      <div className="flex gap-10 mt-15">
-        <div className="order-1">
-          <div className="flex flex-col gap-5">
-            <h1>Creative Portfolio</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-              autem, quo voluptatem eligendi repellendus possimus quibusdam.
-              Corrupti debitis at accusamus.
-            </p>
+          <div className="w-100 lg:h-50 h-40 relative max-sm:hidden">
+            <Image
+              src="https://cdn.pixabay.com/photo/2016/11/29/08/58/hands-1868562_1280.jpg"
+              alt="image"
+              fill={true}
+              className=""
+            />
           </div>
         </div>
-
-        <div className="w-100 lg:h-50 h-40 relative max-sm:hidden">
-          <Image
-            src="https://cdn.pixabay.com/photo/2016/11/29/08/58/hands-1868562_1280.jpg"
-            alt="image"
-            fill={true}
-            className=""
-          />
-        </div>
-      </div>
+      </Link>
     </div>
   );
 };
